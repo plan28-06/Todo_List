fe81dcf08d883316debde30d0670b3b82738a22b
plan28-06 <planar2006@gmail.com>
2024-07-26 21:06:55 +0530

    Added buttons to the sidebar.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 const menubar = document.querySelector('.menusvg');
 const sidebar = document.querySelector('.sidebar')
 const maincontent = document.querySelector('.maincontent')
+const sidebarbuttons = document.querySelectorAll('.buttons>button')
+
+// To make sidebar open and collapse when menu button is pressed
 menubar.addEventListener('click',function(){
     sidebar.classList.toggle('visible')
     maincontent.classList.toggle('adjustmaincontent')
 })
+
+for (let button of sidebarbuttons){
+    button.addEventListener('click',function(e){
+        if (this.classList[1][1]=='0'){
+            sidebarbuttons[2].classList.remove('active');
+            sidebarbuttons[1].classList.remove('active');
+        }
+        if (this.classList[1][1]=='1'){
+            sidebarbuttons[0].classList.remove('active');
+            sidebarbuttons[2].classList.remove('active');
+        } 
+        if (this.classList[1][1]=='2'){
+            sidebarbuttons[0].classList.remove('active');
+            sidebarbuttons[1].classList.remove('active');
+        }
+        button.classList.add('active');
+    })
+}
